1dc694e53df01dfdb03bbf18b007df402dc89bbf
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-02-15 20:26:26 +0530

    [Saif Ali] Add : Create Linked List Added

---
+++ LinkedListProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.ReadLine();
+        }
+    }
+}
