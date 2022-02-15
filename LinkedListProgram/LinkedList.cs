30b7b9db45fd00e1abbda0d9955baa3a11e7c087
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-02-15 21:01:39 +0530

    [Saif Ali] Add : Abiltiy To insert 30 between 56 and 70 Added

--- LinkedListProgram/LinkedList.cs
+++ LinkedListProgram/LinkedList.cs
@@ -41,5 +41,36 @@ namespace LinkedListProgram
                 temp = temp.next;
             }
         }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if(position < 1)
+            {
+                Console.WriteLine("Invalid Position!");
+            }
+            if(position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while(position-- != 0)
+                {
+                    if(position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if(position != 1)
+                { Console.WriteLine("Position out of range"); }
+            }
+            Console.WriteLine("Inserted value is : " +  data);
+            return head;
+        }
     }
 }
--- LinkedListProgram/Program.cs
+++ LinkedListProgram/Program.cs
@@ -12,8 +12,8 @@ namespace LinkedListProgram
         {
             LinkedList list = new LinkedList();
             list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.InsertAtParticularPosition(2,30);
             list.Display();
             Console.ReadLine();
         }
